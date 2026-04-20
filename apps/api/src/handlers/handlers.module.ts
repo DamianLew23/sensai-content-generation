d0cb7fc58eea8f18bea2c86e3278f8c9dcdb289a
damian.lew <dlew8912@gmail.com>
2026-04-20 13:39:15 +0200

    feat(api): add SerpFetchHandler (type=tool.serp.fetch) and register in HandlersModule

--- apps/api/src/handlers/handlers.module.ts
+++ apps/api/src/handlers/handlers.module.ts
@@ -1,14 +1,18 @@
 import { Module } from "@nestjs/common";
 import { BriefHandler } from "./brief.handler";
+import { SerpFetchHandler } from "./serp-fetch.handler";
+import { ToolsModule } from "../tools/tools.module";
 import { STEP_HANDLERS, type StepHandler } from "../orchestrator/step-handler";
 
 @Module({
+  imports: [ToolsModule],
   providers: [
     BriefHandler,
+    SerpFetchHandler,
     {
       provide: STEP_HANDLERS,
-      useFactory: (brief: BriefHandler): StepHandler[] => [brief],
-      inject: [BriefHandler],
+      useFactory: (brief: BriefHandler, serp: SerpFetchHandler): StepHandler[] => [brief, serp],
+      inject: [BriefHandler, SerpFetchHandler],
     },
   ],
   exports: [STEP_HANDLERS],
