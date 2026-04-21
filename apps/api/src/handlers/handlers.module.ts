9fd9c520769c85639caabaf6f841a28a00525ed2
damian.lew <dlew8912@gmail.com>
2026-04-21 10:33:41 +0200

    feat(api): register ScrapeFetchHandler + FirecrawlModule

--- apps/api/src/handlers/handlers.module.ts
+++ apps/api/src/handlers/handlers.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { BriefHandler } from "./brief.handler";
 import { SerpFetchHandler } from "./serp-fetch.handler";
+import { ScrapeFetchHandler } from "./scrape-fetch.handler";
 import { ToolsModule } from "../tools/tools.module";
 import { STEP_HANDLERS, type StepHandler } from "../orchestrator/step-handler";
 
@@ -9,10 +10,15 @@ import { STEP_HANDLERS, type StepHandler } from "../orchestrator/step-handler";
   providers: [
     BriefHandler,
     SerpFetchHandler,
+    ScrapeFetchHandler,
     {
       provide: STEP_HANDLERS,
-      useFactory: (brief: BriefHandler, serp: SerpFetchHandler): StepHandler[] => [brief, serp],
-      inject: [BriefHandler, SerpFetchHandler],
+      useFactory: (
+        brief: BriefHandler,
+        serp: SerpFetchHandler,
+        scrape: ScrapeFetchHandler,
+      ): StepHandler[] => [brief, serp, scrape],
+      inject: [BriefHandler, SerpFetchHandler, ScrapeFetchHandler],
     },
   ],
   exports: [STEP_HANDLERS],
